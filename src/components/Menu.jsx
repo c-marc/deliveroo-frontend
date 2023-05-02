f1173410857bace0f7e55cbc53e394bc5aefc2c1
Marc C <mcar176@gmail.com>
2023-05-02 14:14:08 +0200

    Fix empty cat

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -4,6 +4,7 @@ const Menu = ({ menu }) => {
   return (
     <div className="menu left-column">
       {menu.map((category) => {
+        if (category.meals.length === 0) return null;
         return <Category key={category.name} category={category} />;
       })}
     </div>
