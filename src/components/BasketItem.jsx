b485c7899c153b0b97d966c123cb217603d5d3f0
Marc C <mcar176@gmail.com>
2023-05-02 17:47:22 +0200

    Fix basket css

--- src/components/BasketItem.jsx
+++ src/components/BasketItem.jsx
@@ -4,14 +4,13 @@ const BasketItem = ({ item, handleQuantityChange }) => {
   const total = Number(item.price) * item.quantity;
   return (
     <li className="basket-item basket-line">
-      <div>
-        <div className="controls">
-          <button onClick={() => handleQuantityChange(item.id, -1)}>-</button>
-          <span>{item.quantity}</span>
-          <button onClick={() => handleQuantityChange(item.id, 1)}>+</button>
-        </div>
-        <span className="basket-item-title">{item.title}</span>
+      <div className="controls">
+        <button onClick={() => handleQuantityChange(item.id, -1)}>-</button>
+        <span>{item.quantity}</span>
+        <button onClick={() => handleQuantityChange(item.id, 1)}>+</button>
       </div>
+
+      <span className="basket-item-title">{item.title}</span>
 
       <span className="price">{asPrice(total)}</span>
     </li>
